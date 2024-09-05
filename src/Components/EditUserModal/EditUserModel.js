11f238daabbc3c651f9c4114ec77a8fb3756e51b
Mohammed Zeeshan Khan <zeeshankhan47414@gmail.com>
2024-09-05 19:44:38 +0530

    completed developing edit users modal

--- src/Components/EditUserModal/EditUserModel.js
+++ src/Components/EditUserModal/EditUserModel.js
@@ -6,14 +6,29 @@ const EditUserModel = ({ setIsEditVisible, name }) => {
         <div className={`${styles.modalContainer}`} >
             <div className={`${styles.editUContainer}`}>
                 <h2>Modify Users for : {name}</h2>
-                <form >
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-                    <label for="vehicle1"> I have a bike</label><br />
-                    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
-                    <label for="vehicle2"> I have a car</label><br />
-                    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
-                    <label for="vehicle3"> I have a boat</label><br /><br />
-                    <input type="submit" value="Submit" />
+                <form className={`${styles.checkboxCon}`}>
+                    <div> <input type="checkbox" id="user1" name="user1" value="Bike" className={`${styles.checkBox}`} />
+                        <label className={`${styles.label}`} for="user1"> User1</label> </div>
+                    <div> <input type="checkbox" id="user2" name="user2" value="Car" className={`${styles.checkBox}`} />
+                        <label className={`${styles.label}`} for="user2"> User2</label> </div>
+                    <div> <input type="checkbox" id="User3" name="user3" value="Boat" className={`${styles.checkBox}`} />
+                        <label className={`${styles.label}`} for="User3"> User3</label> </div>
+                    <div> <input type="checkbox" id="user4" name="user4" value="Car" className={`${styles.checkBox}`} />
+                        <label className={`${styles.label}`} for="user4"> User4</label> </div>
+                    <div> <input type="checkbox" id="user5" name="user5" value="Car" className={`${styles.checkBox}`} />
+                        <label className={`${styles.label}`} for="user5"> User5</label> </div>
+                    <div> <input type="checkbox" id="user6" name="user6" value="Car" className={`${styles.checkBox}`} />
+                        <label className={`${styles.label}`} for="user6"> User6</label> </div>
+                    <div className={`${styles.btnWrapper}`}>
+                        <button className={`${styles.cnlBtn}`} onClick={(e) => {
+                            e.preventDefault();
+                            setIsEditVisible(false);
+                        }}>Cancel</button>
+                        <button className={`${styles.submitBtn}`} onClick={(e) => {
+                            e.preventDefault();
+                        }}>Save</button>
+                    </div>
+
                 </form>
             </div>
 
